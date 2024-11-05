42339f041fc52d8961c507c11db8a99c7b6d63c1
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2024-11-05 21:02:03 +0100

    add catpuccin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    fontFamily: {
-      sans: ['"Arial"', "sans-serif"],
-    },
-    extend: {
-      colors: {
-        background: "#e0e0e0",
-        "background-dark": "#202020",
-      },
-    },
-  },
-  plugins: [],
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      prefix: "ctp",
+    }),
+  ],
 };
